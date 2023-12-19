6c097f1d3c74c427776050630d2849a45e54bcdf
seilylook <seilylook@naver.com>
2023-12-19 19:59:32 +0900

    feat: schedule form

--- lib/widgets/schedule_bottom_sheet.dart
+++ lib/widgets/schedule_bottom_sheet.dart
@@ -1,3 +1,5 @@
+import 'package:calendar_scheduler/constants/constants.dart';
+import 'package:calendar_scheduler/widgets/custom_text_field.dart';
 import 'package:flutter/material.dart';
 
 class ScheduleBottomSheet extends StatefulWidget {
@@ -10,11 +12,57 @@ class ScheduleBottomSheet extends StatefulWidget {
 class _ScheduleBottomSheetState extends State<ScheduleBottomSheet> {
   @override
   Widget build(BuildContext context) {
+    final bottomInset = MediaQuery.of(context).viewInsets.bottom;
+
     return SafeArea(
       child: Container(
-        height: MediaQuery.of(context).size.height / 2,
+        height: MediaQuery.of(context).size.height / 2 + bottomInset,
         color: Colors.white,
+        child: Padding(
+          padding:
+              EdgeInsets.only(left: 8, right: 8, top: 8, bottom: bottomInset),
+          child: Column(
+            children: [
+              const Row(
+                children: [
+                  Expanded(
+                    child: CustomTextField(
+                      label: '시작 시간',
+                      isTime: true,
+                    ),
+                  ),
+                  SizedBox(width: 16.0),
+                  Expanded(
+                    child: CustomTextField(
+                      label: '종료 시간',
+                      isTime: true,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 8.0),
+              const Expanded(
+                child: CustomTextField(
+                  label: '내용',
+                  isTime: false,
+                ),
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: onSavePressed,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: PRIMARY_COLOR,
+                  ),
+                  child: const Text('저장'),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
+
+  void onSavePressed() {}
 }
