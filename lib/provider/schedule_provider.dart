34eeb09148e026768f96ae72f09fb08e639c611a
seilylook <seilylook@naver.com>
2023-12-21 16:32:18 +0900

    add: error handling

--- lib/provider/schedule_provider.dart
+++ lib/provider/schedule_provider.dart
@@ -3,6 +3,7 @@ import 'package:calendar_scheduler/apis/schedule_api.dart';
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:uuid/uuid.dart';
 
 class ScheduleProvider extends ChangeNotifier {
   final ScheduleAPI api;
@@ -27,35 +28,65 @@ class ScheduleProvider extends ChangeNotifier {
 
   void createSchedule({required ScheduleModel schedule}) async {
     final targetDate = schedule.date;
+    final uuid = Uuid();
+    final tempId = uuid.v4();
+    final newSchedule = schedule.copyWith(id: tempId);
     final savedSchedule = await api.createSchedule(schedule: schedule);
 
     cache.update(
       targetDate,
       (value) => [
         ...value,
-        schedule.copyWith(
-          id: savedSchedule,
-        ),
+        newSchedule,
       ]..sort(
           (a, b) => a.startTime.compareTo(
             b.startTime,
           ),
         ),
-      ifAbsent: () => [schedule],
+      ifAbsent: () => [newSchedule],
     );
 
     notifyListeners();
+
+    try {
+      final savedSchedule = await api.createSchedule(schedule: schedule);
+
+      cache.update(
+        targetDate,
+        (value) => value
+            .map((e) => e.id == tempId
+                ? e.copyWith(
+                    id: savedSchedule,
+                  )
+                : e)
+            .toList(),
+      );
+    } catch (e) {
+      cache.update(
+          targetDate, (value) => value.where((e) => e.id != tempId).toList());
+    }
   }
 
   void deleteSchedule({required DateTime date, required String id}) async {
-    final response = await api.deleteSchedule(id: id);
+    final targetSchedule =
+        cache[date]!.firstWhere((element) => element.id == id);
 
     cache.update(
       date,
-      (value) => value.where((e) => e.id != id).toList(),
+      (value) => value.where((element) => element.id != id).toList(),
       ifAbsent: () => [],
     );
 
+    try {
+      await api.deleteSchedule(id: id);
+    } catch (e) {
+      cache.update(
+        date,
+        (value) => [...value, targetSchedule]..sort(
+            (a, b) => a.startTime.compareTo(b.startTime),
+          ),
+      );
+    }
     notifyListeners();
   }
 
