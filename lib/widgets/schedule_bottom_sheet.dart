404d5648dd4280e7126f3a91c0ed9b71af1d47ac
seilylook <seilylook@naver.com>
2023-12-21 15:25:45 +0900

    refactor: change to provider

--- lib/widgets/schedule_bottom_sheet.dart
+++ lib/widgets/schedule_bottom_sheet.dart
@@ -1,9 +1,12 @@
 import 'package:calendar_scheduler/constants/constants.dart';
+import 'package:calendar_scheduler/model/schedule_model.dart';
+import 'package:calendar_scheduler/provider/schedule_provider.dart';
 import 'package:calendar_scheduler/widgets/custom_text_field.dart';
 import 'package:flutter/material.dart';
 import 'package:drift/drift.dart' hide Column;
 import 'package:get_it/get_it.dart';
 import 'package:calendar_scheduler/database/drift_database.dart';
+import 'package:provider/provider.dart';
 
 class ScheduleBottomSheet extends StatefulWidget {
   final DateTime selectedDate;
@@ -72,7 +75,7 @@ class _ScheduleBottomSheetState extends State<ScheduleBottomSheet> {
                     ),
                   ],
                 ),
-                SizedBox(height: 8.0),
+                const SizedBox(height: 8.0),
                 Expanded(
                   child: CustomTextField(
                     // 내용 입력 필드
@@ -88,13 +91,11 @@ class _ScheduleBottomSheetState extends State<ScheduleBottomSheet> {
                 SizedBox(
                   width: double.infinity,
                   child: ElevatedButton(
-                    // [저장] 버튼
-                    // ➌ [저장] 버튼
-                    onPressed: onSavePressed,
+                    onPressed: () => onSavePressed(context),
                     style: ElevatedButton.styleFrom(
                       primary: PRIMARY_COLOR,
                     ),
-                    child: Text('저장'),
+                    child: const Text('저장'),
                   ),
                 ),
               ],
@@ -105,20 +106,19 @@ class _ScheduleBottomSheetState extends State<ScheduleBottomSheet> {
     );
   }
 
-  void onSavePressed() async {
+  void onSavePressed(BuildContext context) async {
     if (formKey.currentState!.validate()) {
-      // ➊ 폼 검증하기
-      formKey.currentState!.save(); // ➋ 폼 저장하기
+      formKey.currentState!.save();
 
-      await GetIt.I<LocalDatabase>().createSchedule(
-        // ➊ 일정 생성하기
-        SchedulesCompanion(
-          startTime: Value(startTime!),
-          endTime: Value(endTime!),
-          content: Value(content!),
-          date: Value(widget.selectedDate),
-        ),
-      );
+      context.read<ScheduleProvider>().createSchedule(
+            schedule: ScheduleModel(
+              id: 'new_model',
+              content: content!,
+              date: widget.selectedDate,
+              startTime: startTime!,
+              endTime: endTime!,
+            ),
+          );
 
       Navigator.of(context).pop();
     }
