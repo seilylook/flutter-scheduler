54d22fc2334a352fec97f6a3601bea1c70664c5a
seilylook <seilylook@naver.com>
2023-12-22 14:22:43 +0900

    config: firebase set up

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,16 @@ import 'package:intl/date_symbol_data_local.dart';
 import 'package:calendar_scheduler/database/drift_database.dart';
 import 'package:get_it/get_it.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:calendar_scheduler/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding();
+
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // );
+
   await initializeDateFormatting();
   final database = LocalDatabase();
   GetIt.I.registerSingleton<LocalDatabase>(database);
